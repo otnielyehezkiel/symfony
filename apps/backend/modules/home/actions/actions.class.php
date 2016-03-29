520834807ccd7b12ca374d20875e55d9a46e5823
Achmad Saiful <achmads123@gmail.com>
2016-03-29 20:59:16 +0700

    Styling

--- apps/backend/modules/home/actions/actions.class.php
+++ apps/backend/modules/home/actions/actions.class.php
@@ -24,7 +24,30 @@ class homeActions extends sfActions
     }
   }
 
-  public function executeGetMaterial(sfWebRequest $request){
+  public function executeMaterial(sfWebRequest $request)
+  {
+    if(true) {
+        return sfView::SUCCESS;    
+    } else {
+      return sfView::ERROR;
+    }
+  }
+
+  public function executeSubmitMaterial(sfWebRequest $request){
+    $this->matnumber = $request->getParameter('matnumber');
+    $this->mattype = $request->getParameter('mattype');
+    $this->isec = $request->getParameter('isec');
+    $this->matdes = $request->getParameter('matdes');
+    $this->bum = $request->getParameter('bum');
+    $this->matgroup = $request->getParameter('matgroup');
+    var_dump($this->matnumber);
+    var_dump($this->mattype);
+    var_dump($this->isec);
+    var_dump($this->matdes);
+    var_dump($this->bum);
+    var_dump($this->matgroup);
+    die();
+
     include_once(sfConfig::get('app_nusoap_dir').'/nusoap.php');
     
     $wsdl = "http://erpdvqa.ptpn10.co.id:8052/sap/bc/srt/wsdl/flv_10002A1112D1/bndg_url/sap/bc/srt/rfc/sap/zws_get_vendor/521/zws_get_vendor/zws_get_vendor?sap-client=521";
@@ -50,6 +73,5 @@ class homeActions extends sfActions
         }
     }
     return sfView::none;  
-    // die();
   }
 }
--- apps/backend/modules/home/templates/materialSuccess.php
+++ apps/backend/modules/home/templates/materialSuccess.php
@@ -6,7 +6,27 @@
 	    <form class="col-md-6" action="submitMaterial" method="POST">
 		  <div class="form-group">
 		    <label for="purchase-requisition">Material Number</label>
-		    <input type="text" class="form-control" id="matnumber" name="matnumber">
+		    <input type="text" class="form-control" id="matnumber" name="matnumber" placeholder="Material Number">
+		  </div>
+		  <div class="form-group">
+		    <label for="purchase-requisition">Material Type</label>
+		    <input type="text" class="form-control" id="mattype" name="mattype" placeholder="Material Type">
+		  </div>
+		  <div class="form-group">
+		    <label for="purchase-requisition">Industry Sector</label>
+		    <input type="text" class="form-control" id="isec" name="isec" placeholder="Industry Sector">
+		  </div>
+		  <div class="form-group">
+		    <label for="purchase-requisition">Material Description</label>
+		    <input type="text" class="form-control" id="matdes" name="matdes" placeholder="Material Description">
+		  </div>
+		  <div class="form-group">
+		    <label for="purchase-requisition">Base Unit of Measure</label>
+		    <input type="text" class="form-control" id="bum" name="bum" placeholder="Base Unit of Measure">
+		  </div>
+		  <div class="form-group">
+		    <label for="purchase-requisition">Material Group</label>
+		    <input type="text" class="form-control" id="matgroup" name="matgroup" placeholder="Material Group">
 		  </div>
 		  <button type="submit" class="btn btn-primary pull-right">Submit</button>
 		</form>
